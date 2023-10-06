e0b827d8e4da609a69133a4b0bc2ac002cfd10c4
rolando-r <131362377+rolando-r@users.noreply.github.com>
2023-10-05 21:53:11 -0500

    feat: :sparkles: skeleton
    Created skeleton

--- API/Program.cs
+++ API/Program.cs
@@ -1,11 +1,36 @@
+using System.Reflection;
+using System.Text;
+using API.Extensions;
+using AspNetCoreRateLimit;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers(options =>
+{
+    options.RespectBrowserAcceptHeader = true;
+    options.ReturnHttpNotAcceptable = true;
+}).AddXmlSerializerFormatters();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.ConfigureRatelimiting();
+ builder.Services.ConfigureApiVersioning(); 
+
+builder.Services.AddJwt(builder.Configuration);
+builder.Services.AddAutoMapper(Assembly.GetEntryAssembly());
+builder.Services.ConfigureCors();
+builder.Services.AddAplicationServices();
+builder.Services.AddDbContext<BaseContext>(options =>
+{
+    string connectionString = builder.Configuration.GetConnectionString("ConexMysql");
+    options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+});
 
 var app = builder.Build();
 
@@ -16,7 +41,12 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseCors("CorsPolicy");
+
 app.UseHttpsRedirection();
+
+app.UseIpRateLimiting();
+app.UseAuthentication();
 
 app.UseAuthorization();
 
